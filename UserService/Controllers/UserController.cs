5aacff488a4682fd35a282b37021cc318b5f4efb
Omegasis <61483070+omegadevsis@users.noreply.github.com>
2025-12-02 00:10:46 -0300

    User Controller V2

--- UserService/Controllers/UserController.cs
+++ UserService/Controllers/UserController.cs
@@ -1,12 +1,24 @@
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UserService.Controllers;
 
+[ApiController]
+[Route("[controller]")]
 public class UserController : Controller
 {
-    // GET
-    public IActionResult Index()
+    [HttpGet]
+    public async Task<ActionResult> Get()
     {
-        return View();
+        try
+        {
+            return Ok("teste ok");
+        }
+        catch (Exception ex)
+        {
+            //logger.LogError(ex, GetType().FullName);
+            return BadRequest(new { Mensagem = "Erro ao requisitar dados" });
+        }
     }
 }
