afe13addcf80aa1b447222a9351b5836fbc9d80a
Omegasis <61483070+omegadevsis@users.noreply.github.com>
2025-12-06 10:36:06 -0300

    Removido url e porta fixadas

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -6,10 +6,7 @@ using ProductService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,22 +14,13 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Urls.Add("http://+:5097");  
-
 app.Run();
+
